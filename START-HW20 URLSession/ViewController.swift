f35704d22e89d13b2d07be743e2735f6b5dfb6b3
Adlet <adlet.zh19966@gmail.com>
2023-05-09 16:30:27 +0600

    Initial Commit

---
+++ START-HW20 URLSession/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  START-HW20 URLSession
+//
+//  Created by Adlet Zhantassov on 09.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
